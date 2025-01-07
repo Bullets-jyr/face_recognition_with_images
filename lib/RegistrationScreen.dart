9e11d76ce226f70c070b410df67eb1987b220fc5
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-01-07 10:55:40 +0900

    build: Adding Face Detection Library in Flutter

--- lib/RegistrationScreen.dart
+++ lib/RegistrationScreen.dart
@@ -207,7 +207,7 @@ class _HomePageState extends State<RegistrationScreen> {
                           color: Colors.blue, size: screenWidth / 7),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           )
